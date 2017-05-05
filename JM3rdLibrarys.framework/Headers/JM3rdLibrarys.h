2e6c8e45331a471a0095644f89987f74c3e8db7b
atoneJM <omniscientjoker@users.noreply.github.com>
2017-05-05 16:28:30 +0800

    Add files via upload

---
+++ JM3rdLibrarys.framework/Headers/JM3rdLibrarys.h
@@ -0,0 +1,19 @@
+//
+//  JM3rdLibrarys.h
+//  JM3rdLibrarys
+//
+//  Created by joker on 2017/5/5.
+//  Copyright © 2017年 joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JM3rdLibrarys.
+FOUNDATION_EXPORT double JM3rdLibrarysVersionNumber;
+
+//! Project version string for JM3rdLibrarys.
+FOUNDATION_EXPORT const unsigned char JM3rdLibrarysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JM3rdLibrarys/PublicHeader.h>
+
+
